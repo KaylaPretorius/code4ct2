689dd60544658f41102ea6cc10c0619f00b3f3bc
Kayla <kayla.pretorius16@gmail.com>
2018-07-20 15:28:16 +0200

    resubmit

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,28 +1,88 @@
+// This is for hamburger menu
+var nav = document.getElementById("topNav");
+var main = document.getElementById("main");
+var menu = document.getElementsByClassName("menuitems");
+var close = document.getElementById("closebtn");
 
- // Count Up
- function countUp(numberElement){
-   let current = 0;
-   const total = parseInt(numberElement.textContent);
-   const increment = Math.ceil(total/4000);
-   function step(){
-     current += increment;
-     if (current > total) current = total ;
-     numberElement.textContent= current.toLocaleString();
-     (current !== total ) && requestAnimationFrame(step);
-   }
-   step();
- }
- document.querySelectorAll('.number').forEach(elem => countUp(elem));
+//default to measure if/else from
+nav.style.height = "50px";
+// main.style.marginTop = "50px";
+for (i = 0; i < menu.length; i++){menu[i].style.marginTop="100px";};
+
+close.addEventListener("click", function(){
+  var menuIcon = close.children;
+  for (i = 0; i < menuIcon.length; i++){
+    menuIcon[i].classList.toggle("active");
+  }
+});
+
+function navToggle() {
+  //to close
+  if (nav.style.height <= "275px") {
+    nav.style.height = "50px";
+    // main.style.marginTop = "50px";
+
+    var i = 0;
+    for (i = 0; i < menu.length; i++){
+      menu[i].style.opacity="0.0";
+      // menu[i].style.marginTop="100px";
+    };
+
+  }
+  //to open
+  else if (nav.style.height <= "50px") {
+    nav.style.height = "275px";
+    // main.style.marginTop = "275px";
+    var i = 0;
+    for (i = 0; i < menu.length; i++){
+      menu[i].style.opacity="1.0";
+      menu[i].style.marginTop="0px";
+    };
+
+  }
+
+};
+
+// This is for the Slideshow
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+// Count Up
+function countUp(numberElement){
+  let current = 0;
+  const total = parseInt(numberElement.textContent);
+  const increment = Math.ceil(total/4000);
+  function step(){
+    current += increment;
+    if (current > total) current = total ;
+    numberElement.textContent= current.toLocaleString();
+    (current !== total ) && requestAnimationFrame(step);
+  }
+  step();
+}
+document.querySelectorAll('.number').forEach(elem => countUp(elem));
 // Count Up ends here
+
+// this is for the percentage Count up on about page
 
 var canvas = document.getElementsByTagName('canvas');
 
-  for (var i = 0; i < canvas.length; i++) {
+for (var i = 0; i < canvas.length; i++) {
   progressBar(canvas[i].id);
-  }
-
-  // this is for the percentage Count up on about page
-  function progressBar(canvasId) {
+}
+function progressBar(canvasId) {
   var canvas = document.getElementById(canvasId);
   var ctx = canvas.getContext('2d');
 
@@ -81,22 +141,13 @@ var canvas = document.getElementsByTagName('canvas');
     // ctx.fillText(definition, cWidth/2 - outputTextDefinitionWidth/2, cHeight/2 + 15);
   }
   degreesCall = setInterval(getDegrees, 10/(degrees - endDegrees));
-  }
+}
 
-  // the elements are cloned in order for the carousel to move infinitely
-  $(function() {
-    var $first=$('#partners>:first-child');
-    $('#partners').width($first.outerWidth(true));
-    $first.clone(true).appendTo('#partners');
-    $('#partners').addClass('ready');
-  });
-// This is where the percentage Count Up ends
-
-
-// hamburger menu
-
-$(document).ready(function() {
-	$("body").on('click', '.top', function() {
-		$("nav.menu").toggleClass("menu_show");
-	});
+// the elements are cloned in order for the carousel to move infinitely
+$(function() {
+  var $first=$('#partners>:first-child');
+  $('#partners').width($first.outerWidth(true));
+  $first.clone(true).appendTo('#partners');
+  $('#partners').addClass('ready');
 });
+// This is where the percentage Count Up ends
